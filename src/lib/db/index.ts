120c3ad616b4d6650ae34da24195e8a0c156e25b
baiqueee <hughdev101@gmail.com>
2024-08-04 00:52:58 +0700

    disable prefetch drizzle

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,7 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-import { env } from "@/lib/env.mjs";
- 
+import { drizzle } from "drizzle-orm/postgres-js"
+import postgres from "postgres"
+import { env } from "@/lib/env.mjs"
+
 const connectionString = env.DATABASE_URL
-const client = postgres(connectionString)
-export const db = drizzle(client);
+const client = postgres(connectionString, { prepare: false })
+export const db = drizzle(client)
